f64231542d55d682d7d0135b345e451a52a7f045
AgustinPalmaM <palma.agustin7@gmail.com>
2023-03-24 17:37:49 -0300

    Create function getItemsFromAPI to make fetch to our url with the json data with items Create function showItems(responseAPI) to take the response from the the called to the api and display into the containerItems div all of the availables items in the json db

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let customer = {
   order: [],
 };
 
+const categories = {
+  1: "Foods",
+  2: "Drinks",
+  3: "Desserts"
+}
+
 const buttonSaveCustomer = document.querySelector("#guardar-cliente");
 buttonSaveCustomer.addEventListener("click", saveCustomer);
 
@@ -35,12 +41,53 @@ function saveCustomer() {
   modalBootstrap.hide();
 
   showSections();
+
+  getItemsFromAPI();
 }
 
 function showSections() {
   const hidenSections = document.querySelectorAll('.d-none');
   hidenSections.forEach(section => {
     section.classList.remove('d-none');
+  })
+}
+
+function getItemsFromAPI() {
+
+  const url = 'http://localhost:3000/items';
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => showItems(data))
+    .catch(error => console.log(error))
+}
+
+function showItems(responseAPI) {
+  const containerItems = document.querySelector('#platillos .contenido');
+  responseAPI.forEach(item => {
+    const { id, name, price, category } = item;
+
+    const row = document.createElement('DIV');
+    row.classList.add('row', 'py-3', 'border-top');
+    
+    const nameRow = document.createElement('DIV');
+    nameRow.classList.add('col-md-4');
+    nameRow.textContent = name;
+
+    const priceRow = document.createElement('DIV');
+    priceRow.classList.add('col-md-3', 'fw-bold');
+    priceRow.textContent = `$ ${price}`;
+    
+    const categoryRow = document.createElement('DIV');
+    categoryRow.classList.add('col-md-3');
+    categoryRow.textContent = categories[category];
+
+    row.appendChild(nameRow);
+    row.appendChild(priceRow);
+    row.appendChild(categoryRow);
+    
+    containerItems.appendChild(row);
+    
   })
 }
 
